14575e6f9ef723c1c3612a6189b7f5badcee0404
Erel Japco <ereljapco@gmail.com>
2022-07-31 08:34:38 +0800

    day-03/math: log in the console the value of `PI`, and perform `add` and `square`
    - export `PI`, `add`, and `square` from math.js to app.js and log in the
    console the value of `PI`, and perform `add` and `square` with the
    arguments from app.js

---
+++ day-03/math/app.js
@@ -0,0 +1,5 @@
+const math = require('./math');
+
+console.log(math.PI);
+console.log(math.add(13, 11));
+console.log(math.square(6));
---
+++ day-03/math/math.js
@@ -0,0 +1,9 @@
+const PI = 3.14159;
+
+const add = (x, y) => x + y;
+
+const square = (x) => x * x;
+
+module.exports.PI = PI;
+module.exports.add = add;
+module.exports.square = square;

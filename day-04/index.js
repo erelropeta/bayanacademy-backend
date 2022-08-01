496276bb57b23cd4349abc3723774d84418f510a
Erel Japco <ereljapco@gmail.com>
2022-08-01 15:22:02 +0800

    feat(day-04): add home page, about page, and contact page

---
+++ day-04/index.js
@@ -0,0 +1,23 @@
+const express = require('express');
+const app = express();
+const path = require('path');
+const port = 3000;
+
+app.set('view engine', 'ejs');
+app.set('views', path.join(__dirname, 'views'));
+
+app.get('/', (req, res) => {
+  res.render('home');
+});
+
+app.get('/about', (req, res) => {
+  res.render('about');
+});
+
+app.get('/contact', (req, res) => {
+  res.render('contact');
+});
+
+app.listen(port, () => {
+  console.log(`Listening on port ${port}`);
+});

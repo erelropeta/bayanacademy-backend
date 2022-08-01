a21a49e05638e766054cf6bc4be21bf4d5e2765a
Erel Japco <ereljapco@gmail.com>
2022-08-01 15:41:47 +0800

    feat(day-04): generate a random number on the random number page

--- day-04/index.js
+++ day-04/index.js
@@ -7,17 +7,22 @@ app.set('view engine', 'ejs');
 app.set('views', path.join(__dirname, 'views'));
 
 app.get('/', (req, res) => {
-  res.render('home');
+    res.render('home');
 });
 
 app.get('/about', (req, res) => {
-  res.render('about');
+    res.render('about');
 });
 
 app.get('/contact', (req, res) => {
-  res.render('contact');
+    res.render('contact');
+});
+
+app.get('/rand', (req, res) => {
+    const randomNum = Math.floor(Math.random() * 10) + 1;
+    res.render('random', { randomNum });
 });
 
 app.listen(port, () => {
-  console.log(`Listening on port ${port}`);
+    console.log(`Listening on port ${port}`);
 });

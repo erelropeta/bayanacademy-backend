274faefbc60d1587c7670bb95fff1e635a114def
Erel Japco <ereljapco@gmail.com>
2022-08-02 12:07:55 +0800

    perf(day-04): add `header.ejs` to hold the page head

--- day-04/index.js
+++ day-04/index.js
@@ -10,31 +10,37 @@ app.set('view engine', 'ejs');
 app.set('views', path.join(__dirname, 'views'));
 
 app.get('/', (req, res) => {
-    res.render('home');
+    const title = 'Home';
+    res.render('home', { title });
 });
 
 app.get('/about', (req, res) => {
-    res.render('about');
+    const title = 'About';
+    res.render('about', { title });
 });
 
 app.get('/contact', (req, res) => {
-    res.render('contact');
+    const title = 'Contact';
+    res.render('contact', { title });
 });
 
 app.get('/rand', (req, res) => {
+    const title = 'Random';
     const randomNum = Math.floor(Math.random() * 10) + 1;
-    res.render('random', { randomNum });
+    res.render('random', { title, randomNum });
 });
 
 app.get('/r/:subreddit', (req, res) => {
     const { subreddit } = req.params;
     const data = redditData[subreddit];
-    res.render('subreddit', { ...data });
+    const title = `r/${data.name}`;
+    res.render('subreddit', { title, ...data });
 });
 
 app.get('/cats', (req, res) => {
+    const title = 'Cats';
     const cats = ['Muning', 'Chi-chi', 'Garfield', 'Puti', 'Pampu'];
-    res.render('cats', { cats });
+    res.render('cats', { title, cats });
 });
 
 app.listen(port, () => {

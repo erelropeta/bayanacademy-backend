cd3660d9e36602360e5d59ba22a6f01bf5ff2ea9
Erel Japco <ereljapco@gmail.com>
2022-08-02 15:17:56 +0800

    feat(day-05/egg-drop): display thank you message when user use `POST`

--- day-05/egg-drop/index.js
+++ day-05/egg-drop/index.js
@@ -2,12 +2,21 @@ const express = require('express');
 const app = express();
 const port = 3000;
 
+app.use(express.urlencoded({ extended: true }));
+
 app.get('/order', (req, res) => {
     res.send('Get /order');
 });
 
 app.post('/order', (req, res) => {
-    res.send('Post /order');
+    const { type, qty } = req.body;
+    if (qty == 1) {
+        res.send(`Here is your ${qty} ${type} egg drop. Thank you!`);
+    } else if (qty > 1) {
+        res.send(`Here are your ${qty} ${type} egg drop. Thank you!`);
+    } else {
+        res.send(`You didn't order an egg drop.`);
+    }
 });
 
 app.listen(port, () => {
